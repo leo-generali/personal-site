b197ee0c0f839d517f8435c51047610e414e5574
Leo Generali <leogen17@gmail.com>
2019-05-17 09:31:17 -0400

    Add html-minify into it's own script

--- .eleventy.js
+++ .eleventy.js
@@ -8,37 +8,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(inclusiveLangPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  // Minify HTML
-  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
-    if (outputPath.endsWith('.html')) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
+  // Content Transforms
+  eleventyConfig.addTransform('htmlmin', require('./scripts/html-minify'))
 
-    return content;
-  });
-
-  // prettier-ignore
+  // Collections
   eleventyConfig.addCollection('tagsList', require('./scripts/get-all-tags'));
   eleventyConfig.addCollection('postsByYear', require('./scripts/posts-by-year'))
-
-  // // Group all blog posts by year
-  // eleventyConfig.addCollection('postsByYear', (collection) => {
-  //   const posts = collection
-  //     .getAllSorted()
-  //     .filter((item) => item.inputPath.match(/^\.\/writing\//) !== null)
-  //     .reverse();
-
-  //   const groupedPosts = groupBy(posts, (post) => post.date.getFullYear());
-
-  //   return Object.entries(groupedPosts);
-  // });
-
-
 
   // Copy over favicon to build site
   eleventyConfig.addPassthroughCopy('favicon.ico');
---
+++ scripts/html-minify.js
@@ -0,0 +1,12 @@
+module.exports = (collection) => {
+  if (outputPath.endsWith('.html')) {
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true
+    });
+    return minified;
+  }
+
+  return content;
+}
