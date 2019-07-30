29bb76e0097839161a8ad20bbaf8f227862f4b5a
Leo Generali <leogen17@gmail.com>
2019-07-30 14:09:40 -0400

    Only show published posts in production

--- src/collections/posts-by-year.js
+++ src/collections/posts-by-year.js
@@ -3,6 +3,13 @@ const groupBy = require('lodash.groupby');
 module.exports = (collection) => {
   const posts = collection
     .getFilteredByGlob('src/writing/**/*.md')
+    .filter((post) => {
+      if (process.env.ELEVENTY_ENV === 'production') {
+        return post.data.published;
+      }
+
+      return true;
+    })
     .map((post) => {
       const { published } = post.data;
       post.data.title = published
