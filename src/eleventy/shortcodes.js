cee7888a22672046bbdcee2a69a83283fb785031
Leo Generali <leogen17@gmail.com>
2020-03-26 11:53:57 -0400

    Homepage is now all tailwind

--- src/eleventy/shortcodes.js
+++ src/eleventy/shortcodes.js
@@ -2,10 +2,8 @@ const link = (text, url, args) => {
   return `
     <a 
       href='${url}' 
-      class='text-indigo-400 font-semibold relative
-      hover:text-indigo-600'>
-      ${text}
-    </a>`;
+      class='text-indigo-400 relative
+      hover:text-indigo-600'>${text}</a>`;
 };
 
 module.exports = { link };
