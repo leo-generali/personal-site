5ad1ec91dafd17375f6147e1aa6818da2664e7f1
Leo Generali <leogen17@gmail.com>
2020-04-17 10:23:16 -0400

    Add social share auto generation

---
+++ src/_eleventy/generate-og-images.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+const glob = require("fast-glob");
+const fs = require("fs");
+const rimraf = require("rimraf");
+
+(async () => {
+  const urls = await glob.sync("_site/writing/*/*/og-image/index.html");
+  // .map((path) => path.match(new RegExp("_site/" + "(.*)" + "index.html"))[1]);
+
+  const createImagesPromise = new Promise((resolve, reject) => {
+    urls.forEach(async (url, index) => {
+      // Turn on puppeteer and create a page
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      // Set the viewport of our headless browser to the size
+      // the images
+      await page.setViewport({
+        width: 600,
+        height: 315,
+        deviceScaleFactor: 2,
+      });
+
+      // Get the content of each HTML og-image
+      const fullPath = `${process.cwd()}/${url}`;
+      const content = fs.readFileSync(fullPath, "utf8");
+
+      // Set the content in our browser and take an image.
+      // Save the image next to our written content
+      const imagePath = url.replace("/index.html", ".png");
+      await page.setContent(content);
+      await page.screenshot({
+        path: imagePath,
+      });
+
+      // Close the brows
+      await browser.close();
+
+      if (index === urls.length - 1) resolve();
+    });
+  });
+
+  createImagesPromise.then(() => {
+    rimraf("_site/writing/*/*/og-image", () => {});
+  });
+})();
